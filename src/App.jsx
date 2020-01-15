b06f7be1c0fb0bc01ce9c2dcb573d0342458b78d
lDoughnutl <sam_harvey98@live.com.au>
2020-01-15 14:02:29 +1000

    Created a services page and added a route to app.jsx and lorem ipsum

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import About from "./About"
+import Services from "./Services"
 
 function App() {
 
 return(
   <BrowserRouter>
     <Switch>
+      <Route exact  path="/services" component={Services}></Route>
       <Route exact path="/about" component={About} />
     </Switch>
   </BrowserRouter>
--- src/Services.jsx
+++ src/Services.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const About = () => (
+const Services = () => (
   <div>
-    <h1>About Us</h1>
+    <h1>Our Services</h1>
     <p>Lorem Ipsum Dolor Sit Amet</p>
   </div>
 );
 
-export default About;
+export default Services;
