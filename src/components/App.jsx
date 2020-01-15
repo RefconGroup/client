76251d41c87ab8d6c0755ca5198f0a214c42363d
sizsan <sizsan2001@gmail.com>
2020-01-15 18:30:54 +1000

    serviceServices created

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import About from "./About"
 import Services from "./Services"
---
+++ src/services/serviceServices.js
@@ -0,0 +1,9 @@
+export const getAllServices = async () => {
+    try {
+        const response = await api.get("/")
+        return response.data
+    } catch (error) {
+        console.log("got an error from the server fetching services", error)
+        throw(error)
+    }  
+  }
