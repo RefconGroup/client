bc990258a7712c2c38a96c7f7d847101103e8866
lDoughnutl <sam_harvey98@live.com.au>
2020-01-16 15:15:38 +1000

    A small amoutnt of code cleanup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Container, Row, Col } from 'reactstrap';
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
