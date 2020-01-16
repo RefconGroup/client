157e4f4b7ca0dc0326d7387b8175201ac81840f6
Goste64 <Aidanhamilton@live.com>
2020-01-16 10:30:12 +1000

    created card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Container, Row, Col } from 'reactstrap';
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
 import { RefconNav } from './Nav';
 import RefconCarousell from './Carousell';
+import { RefconCard } from './Card';
 
 function App(){
 return(
---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+export const RefconCard = (props) => {
+  return (
+    <div>
+      <Card>
+        <CardImg top width="100%" src="https://workiz.com/wp-content/uploads/2019/11/Screen-Shot-2019-11-13-at-21.36.23.png" alt="Card image cap" />
+        <CardBody>
+          <CardTitle>Why us?</CardTitle>
+          <CardSubtitle>We are a very COOL company aha</CardSubtitle>
+          <CardText>please buy our airconditioners im going to lose the house</CardText>
+          <Button>Button</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default RefconCard;
