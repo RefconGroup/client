7f7ec2412177d5b31272b116b7c661447cfe5ceb
sizsan <sizsan2001@gmail.com>
2020-01-22 13:48:20 +1000

    Updates

---
+++ precache-manifest.b83fb11170046d3224fbdff44d02d515.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a02c6ebbda75a1c2fb28d7853af1f6d",
+    "url": "/client/index.html"
+  },
+  {
+    "revision": "a609e031c865543194f9",
+    "url": "/client/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a609e031c865543194f9",
+    "url": "/client/static/js/2.a6ec4f58.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/client/static/js/2.a6ec4f58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "648696f9f6b69b206603",
+    "url": "/client/static/js/main.c6ea3153.chunk.js"
+  },
+  {
+    "revision": "017b7a913d32a664bf24",
+    "url": "/client/static/js/runtime-main.2edbc1ea.js"
+  }
+]);
