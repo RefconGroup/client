4cc384295968a9d5e1791fa4fcc332b4fc636fad
lDoughnutl <sam_harvey98@live.com.au>
2020-01-22 15:46:59 +1000

    set baseurl to / for github pages

--- src/components/App.js
+++ src/components/App.js
@@ -46,7 +46,7 @@ function App(){
 
 
 return(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter basename="/">
     <RefconNav />
       <Switch>
         <Route exact path="/" component={Home} />
